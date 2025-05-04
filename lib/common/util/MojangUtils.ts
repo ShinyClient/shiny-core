d2dcb1258ab90617ccdd51e95aa291debef62a40
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2025-05-04 10:06:48 +0545

    pretty

--- lib/common/util/MojangUtils.ts
+++ lib/common/util/MojangUtils.ts
@@ -1,60 +1,60 @@
-import { Rule, Natives } from '../../dl/mojang/MojangTypes'
+import { Rule, Natives } from '../../dl/mojang/MojangTypes';
 
 export function getMojangOS(): string {
-    const opSys = process.platform
-    switch(opSys) {
+    const opSys = process.platform;
+    switch (opSys) {
         case 'darwin':
-            return 'osx'
+            return 'osx';
         case 'win32':
-            return 'windows'
+            return 'windows';
         case 'linux':
-            return 'linux'
+            return 'linux';
         default:
-            return opSys
+            return opSys;
     }
 }
 
 export function validateLibraryRules(rules?: Rule[]): boolean {
-    if(rules == null) {
-        return false
+    if (rules == null) {
+        return false;
     }
-    for(const rule of rules){
-        if(rule.action != null && rule.os != null){
-            const osName = rule.os.name
-            const osMoj = getMojangOS()
-            if(rule.action === 'allow'){
-                return osName === osMoj
-            } else if(rule.action === 'disallow'){
-                return osName !== osMoj
+    for (const rule of rules) {
+        if (rule.action != null && rule.os != null) {
+            const osName = rule.os.name;
+            const osMoj = getMojangOS();
+            if (rule.action === 'allow') {
+                return osName === osMoj;
+            } else if (rule.action === 'disallow') {
+                return osName !== osMoj;
             }
         }
     }
-    return true
+    return true;
 }
 
 export function validateLibraryNatives(natives?: Natives): boolean {
-    return natives == null ? true : Object.hasOwnProperty.call(natives, getMojangOS())
+    return natives == null ? true : Object.hasOwnProperty.call(natives, getMojangOS());
 }
 
 export function isLibraryCompatible(rules?: Rule[], natives?: Natives): boolean {
-    return rules == null ? validateLibraryNatives(natives) : validateLibraryRules(rules)
+    return rules == null ? validateLibraryNatives(natives) : validateLibraryRules(rules);
 }
 
 /**
  * Returns true if the actual version is greater than
  * or equal to the desired version.
- * 
+ *
  * @param {string} desired The desired version.
  * @param {string} actual The actual version.
  */
 export function mcVersionAtLeast(desired: string, actual: string): boolean {
-    const des = desired.split('.')
-    const act = actual.split('.')
+    const des = desired.split('.');
+    const act = actual.split('.');
 
-    for(let i=0; i<des.length; i++){
-        if(!(parseInt(act[i]) >= parseInt(des[i]))){
-            return false
+    for (let i = 0; i < des.length; i++) {
+        if (!(parseInt(act[i]) >= parseInt(des[i]))) {
+            return false;
         }
     }
-    return true
+    return true;
 }
